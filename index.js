6905d15de6801150e319e5e666002859f4bd045b
chemdemo <yangdemo@gmail.com>
2016-12-06 16:04:15 +0800

    init project

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: dmyang
+* @Date:   2016-12-06 15:56:30
+* @Last Modified by:   dmyang
+* @Last Modified time: 2016-12-06 15:59:29
+*/
+
+'use strict'
+
+module.exports = require('./lib/push')

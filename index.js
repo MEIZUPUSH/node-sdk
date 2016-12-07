1ccf7c91e84f1cfa0388afabaad8a40281105c36
chemdemo <yangdemo@gmail.com>
2016-12-07 17:18:44 +0800

    add core code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 * @Author: dmyang
 * @Date:   2016-12-06 15:56:30
 * @Last Modified by:   dmyang
-* @Last Modified time: 2016-12-06 15:59:29
+* @Last Modified time: 2016-12-06 17:42:01
 */
 
 'use strict'
